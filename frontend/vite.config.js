0064ae616eda1a75eb22901251a1271f6c6a2b4b
Andrés Vásquez <avasquez.externo@sii.cl>
2025-12-10 08:37:24 -0300

    Add ioio.cl to Vite allowed hosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
+    allowedHosts: ['ioio.cl', 'www.ioio.cl', 'localhost'],
     watch: {
       usePolling: true
     }
